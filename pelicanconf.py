0fd7e375f9e9bb09d3ee80237a62e8e583e8ac84
Sohom Bhattacharjee <soham.bhattacharjee@gmail.com>
2018-01-03 01:59:28 +0530

    fixed regex metadata

--- pelicanconf.py
+++ pelicanconf.py
@@ -14,21 +14,17 @@ TIMEZONE = 'Asia/Kolkata'
 DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
-# FEED_ALL_ATOM = '/feeds/all.atom.xml'
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
-
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
 
 # Social widget
 SOCIAL = (('Twitter', 'https://twitter.com/sohom154')  ,
           ('Github', 'https://github.com/83bytes') ,
           ('Gitlab', 'https://gitlab.com/83bytes') )
 
+# Pagination
 DEFAULT_PAGINATION = 10
 
+# Plugins
 PLUGINS = ["render_math"]
 PLUGIN_PATHS = ["plugins", "/mnt/data/work_space/blog/pelican/pelican-plugins"]
 
@@ -42,3 +38,7 @@ ARTICLE_URL = 'blog/{slug}.html'
 ARTICLE_SAVE_AS='blog/{slug}.html'
 PAGE_URL = '{slug}.html'
 PAGE_SAVE_AS='{slug}.html'
+
+
+# Metadata regex
+FILENAME_METADATA = '(?P<date>\d{4}-\d{2}-\d{2})-(?P<slug>.*)'
