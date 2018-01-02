134b1f5a6af0a4f7d0d20f0c6da6357126af7a64
Sohom Bhattacharjee <soham.bhattacharjee@gmail.com>
2018-01-03 01:47:14 +0530

    fixed urls

--- pelicanconf.py
+++ pelicanconf.py
@@ -14,16 +14,13 @@ TIMEZONE = 'Asia/Kolkata'
 DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
+# FEED_ALL_ATOM = '/feeds/all.atom.xml'
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-# LINKS = (('SystemrebootV', 'http://getpelican.com/'),
-#          ('Python.org', 'http://python.org/'),
-#          ('Jinja2', 'http://jinja.pocoo.org/'),)
 
 # Social widget
 SOCIAL = (('Twitter', 'https://twitter.com/sohom154')  ,
@@ -38,5 +35,10 @@ PLUGIN_PATHS = ["plugins", "/mnt/data/work_space/blog/pelican/pelican-plugins"]
 LOAD_CONTENT_CACHE = False
 
 
-AUTHOR_SAVE_AS=''
-CATEGORY_SAVE_AS=''
+# URL settings
+AUTHOR_SAVE_AS = ''
+CATEGORY_SAVE_AS = ''
+ARTICLE_URL = 'blog/{slug}.html'
+ARTICLE_SAVE_AS='blog/{slug}.html'
+PAGE_URL = '{slug}.html'
+PAGE_SAVE_AS='{slug}.html'
