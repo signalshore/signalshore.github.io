6bb3661a5a9aae5156ef9fd82036fe2a5c1fec9c
Sohom Bhattacharjee <soham.bhattacharjee15@gmail.com>
2021-06-20 16:44:52 +0530

    pi-cooling-benchmark

--- pelicanconf.py
+++ pelicanconf.py
@@ -7,10 +7,9 @@ SITENAME = 'SignalShore'
 SITEURL = ''
 RELATIVE_URLS = False
 
-
-
 PATH = 'content'
 STATIC_PATHS = ['assets']
+# STATIC_SAVE_AS = 'blog/{path}'
 
 TIMEZONE = 'Asia/Kolkata'
 
@@ -19,22 +18,30 @@ DEFAULT_LANG = 'en'
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = 'feeds/all.atom'
 
-
 # Links
-LINKS = (('Archives', '/archives.html'),
-         ('Tags', '/tag/index.html'))
+LINKS = (('Archives', '/archives.html'), ('Tags', '/tag/index.html'))
 
-
+MARKDOWN = {
+    'extension_configs': {
+        'markdown.extensions.toc': {},
+        'markdown.extensions.codehilite': {
+            'css_class': 'highlight'
+        },
+        'markdown.extensions.extra': {},
+        'markdown.extensions.meta': {},
+    },
+    'output_format': 'html5',
+}
 # Pagination
 DEFAULT_PAGINATION = 10
 
 # Plugins
-PLUGINS = ["render_math", "org_reader"]
-PLUGIN_PATHS = ["plugins", "/mnt/data/work_space/blog/pelican/pelican-plugins"]
+PLUGINS = ["render_math"]
+PLUGIN_PATHS = ["plugins", "/home/sohom/wkspc/blog/pelican/pelican-plugins"]
 
 # Themes
 # I am using the AboutWilson theme
-THEME = '/mnt/data/work_space/blog/signalshore.github.io/theme/'
+THEME = '/home/sohom/wkspc/blog/signalshore.github.io/theme/'
 
 LOAD_CONTENT_CACHE = False
 ORG_READER_EMACS_LOCATION = '/usr/bin/emacs'
@@ -45,11 +52,10 @@ CATEGORIES_SAVE_AS = ''
 AUTHOR_SAVE_AS = ''
 CATEGORY_SAVE_AS = ''
 
-
 ARTICLE_URL = 'blog/{slug}.html'
-ARTICLE_SAVE_AS='blog/{slug}.html'
+ARTICLE_SAVE_AS = 'blog/{slug}.html'
 PAGE_URL = '{slug}.html'
-PAGE_SAVE_AS='{slug}.html'
+PAGE_SAVE_AS = '{slug}.html'
 TAGS_SAVE_AS = 'tag/index.html'
 
 # Metadata regex
