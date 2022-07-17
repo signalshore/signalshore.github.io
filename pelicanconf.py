26cd325c3953d9b3a984daffdbb74ca349353a0e
Sohom Bhattacharjee <soham.bhattacharjee15@gmail.com>
2022-07-18 02:33:16 +0530

    changed URL for entire site. added yearly and monthly archives

--- pelicanconf.py
+++ pelicanconf.py
@@ -52,11 +52,17 @@ CATEGORIES_SAVE_AS = ''
 AUTHOR_SAVE_AS = ''
 CATEGORY_SAVE_AS = ''
 
-ARTICLE_URL = 'blog/{slug}.html'
-ARTICLE_SAVE_AS = 'blog/{slug}.html'
+ARTICLE_URL = 'posts/{date:%Y}/{date:%b}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%b}/{date:%d}/{slug}/index.html'
+
+YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/{date:%b}/index.html'
+
 PAGE_URL = '{slug}/'
 PAGE_SAVE_AS = '{slug}/index.html'
+
 TAGS_SAVE_AS = 'tag/index.html'
+ARCHIVES_SAVE_AS = 'archives/index.html'
 
 # Metadata regex
 FILENAME_METADATA = '(?P<date>\d{4}-\d{2}-\d{2})-(?P<slug>.*)'
